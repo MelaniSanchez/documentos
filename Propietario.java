5c6277d4bbb16ce2f7bd0761af8ef8db0721a2b2
MelaniSanchez <128442299+MelaniSanchez@users.noreply.github.com>
2023-03-20 22:45:56 -0400

    Update Propietario.java

---
+++ Propietario.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+/*
+ * 
+ */
+public class Propietario{
+    private String nombre;
+    private String apellido;
+    
+
+    public Propietario(){
+        nombre ="";
+        apellido="";
+    }
+    /*
+     * 
+     */
+    public void Leer(){
+        //TODO: INGRESAR DATOS DEL PROPIETARIO
+    }
+    /*
+     * 
+     */
+    public void Imprimir(){
+        //TODO: MOSTRAR LOS DATOS DEL PROPIETARIO
+    }
+}
